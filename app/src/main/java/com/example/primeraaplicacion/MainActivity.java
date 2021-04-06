3efb613657859ba3df9bad9855edf44ee50c0bb3
Pedro Ulloa <pulloam@hotmail.com>
2021-04-06 16:08:00 -0400

    Comentario commit para desarrollo

--- app/src/main/java/com/example/primeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/primeraaplicacion/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //Comentario para commit
+    //Comentario para commit desarrollo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
